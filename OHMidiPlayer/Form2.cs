9e2e21bb5b7fba8dd33167281ffd631e4b32f8cb
Frostipanda <162925610+Frostipanda@users.noreply.github.com>
2024-08-08 20:55:37 -0700

    Create Form2.cs

---
+++ OHMidiPlayer/Form2.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace MidiPlayer
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        // This is the method the designer is referencing.
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+            // You can add any initialization code here if needed.
+        }
+    }
+}
